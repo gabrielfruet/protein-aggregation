07d88f9e5fb728b7aa078e8a59b429e178e1e8ba
gabrielfruet <gabrielfruet538@gmail.com>
2025-01-21 17:35:40 -0300

    fix: muting rosetta

--- src/protein/rosetta.py
+++ src/protein/rosetta.py
@@ -6,5 +6,5 @@ def rosetta_init():
     global IS_ROSETTA_INITIALIZED
 
     if not IS_ROSETTA_INITIALIZED:
-        pyrosetta.init()
+        pyrosetta.init("-mute core -mute protocols")
         IS_ROSETTA_INITIALIZED=True
