de91aee36f44bb05673a3c08a792fb91a584aade
mateusribeirog <matribg04@gmail.com>
2025-06-30 17:03:51 -0300

    fix: configured dual docker

--- teste.py
+++ teste.py
@@ -2,7 +2,6 @@ import requests
 import os
 import sys
 
-# Adiciona o diretório src ao path para que possamos importar de lá
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 def run_system_check():
@@ -13,12 +12,11 @@ def run_system_check():
     print("INICIANDO VERIFICAÇÃO DO SISTEMA DE DOIS CONTAINERS")
     print("="*60)
 
-    # --- Teste 1: Validar Container Local (TemBERTure) ---
+    # --- Test 1 ---
     print("\n[TESTE 1/2] Verificando o ambiente local e o TemBERTure...")
     try:
         from src.protein.thermostability.temberture import calculate_temberture_score
 
-        #temberture = TemBERTure(tm_model_path, cls_model_path, half_precision=False)
         test_sequence = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVL"
         
         print("Realizando uma predição com o TemBERTure...")
@@ -32,7 +30,7 @@ def run_system_check():
         print(f"   Erro detalhado: {e}")
         return
 
-    # --- Teste 2: Validar Comunicação com o Serviço ESMFold ---
+    # --- Test 2 ---
     print("\n[TESTE 2/2] Verificando a comunicação com o esmfold-service...")
     try:
         api_url = "http://esmfold-service:5001/predict"
@@ -45,9 +43,7 @@ def run_system_check():
         
         data = response.json()
         
-        # --- AQUI ESTÁ A CORREÇÃO ---
-        # Verificamos se a chave 'pdb' existe, se o valor não está vazio,
-        # e se a palavra "ATOM" aparece no texto (ignorando espaços em branco no início).
+       
         if 'pdb' in data and data['pdb'] and "ATOM" in data['pdb'].strip():
             print("Resposta da API recebida com sucesso!")
             print(f"Início do PDB recebido:\n{data['pdb'][:200]}...")
